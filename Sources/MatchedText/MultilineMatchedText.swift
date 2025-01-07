b67a52f7e9601e0235eb659c14e833e9362c2547
Joseph Wardell <jay@jaywardell.me>
2025-01-07 08:01:16 -0500

    added a preview with line length

--- Sources/MatchedText/MultilineMatchedText.swift
+++ Sources/MatchedText/MultilineMatchedText.swift
@@ -62,3 +62,33 @@ struct MultilineMatchedText: View {
     .padding(.horizontal)
     .frame(width: 150)
 }
+
+#Preview {
+    MultilineMatchedText(text:
+    """
+        I think that I shall never see
+        A poem lovely as a tree.
+        
+        A tree whose hungry mouth is prest
+        Against the earth's sweet flowing breast;
+        
+        A tree that looks at God all day,
+        And lifts her leafy arms to pray;
+        
+        A tree that may in summer wear
+        A nest of robins in her hair;
+        
+        Upon whose bosom snow has lain;
+        Who intimately lives with rain.
+        
+        Poems are made by fools like me,
+        But only God can make a tree.
+        
+        -- Joyce Kilmer
+    """
+     // from https://poets.org/poem/trees
+    )
+    .environment(\.searchFilter, "tree")
+    .environment(\.lineLength, 25)
+    .padding(.horizontal)
+}
