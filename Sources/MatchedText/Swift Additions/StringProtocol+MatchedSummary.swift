bdcbac98010c58cce5d9a35280e5300e20dec116
Joseph Wardell <jay@jaywardell.me>
2025-01-06 22:37:15 -0500

    if the filter is at the beginning of the string, then return the first `length` characters of the string

--- Sources/MatchedText/Swift Additions/StringProtocol+MatchedSummary.swift
+++ Sources/MatchedText/Swift Additions/StringProtocol+MatchedSummary.swift
@@ -7,9 +7,18 @@
 
 extension StringProtocol {
     public func matchedSummary(length: Int, matching filter: any StringProtocol) -> String {
-        guard length > filter.count else { return "" }
-        
+        guard length > filter.count,
+              !filter.isEmpty
+        else { return "" }
+
+        if self.hasPrefix(filter) {
+            let end = Swift.min(length, count)
+            let endIndex = Swift.min(index(startIndex, offsetBy: end), endIndex)
+            return String(self[..<endIndex])
+        }
+
         if self.contains(filter) { return String(self) }
+        
         
         return ""
     }
--- Tests/MatchedTextTests/StringProtocol+MatchedSummaryTests.swift
+++ Tests/MatchedTextTests/StringProtocol+MatchedSummaryTests.swift
@@ -9,30 +9,39 @@ import Testing
 import MatchedText
 
 struct String_MatchedSummary_Test {
-
+    
     @Test func empty_returns_empty() async throws {
         let sut = ""
         
         #expect(sut.matchedSummary(length: 0, matching: "") == "")
     }
-
+    
     @Test func if_length_is_zero_returns_empty() async throws {
         let sut = "cat"
         
         #expect(sut.matchedSummary(length: 0, matching: "cat") == "")
     }
-
+    
     @Test func if_matching_is_empty_returns_empty() async throws {
         let sut = "cat"
         
         #expect(sut.matchedSummary(length: 10, matching: "") == "")
     }
-
+    
     @Test func if_matching_equals_self_returns_filter() async throws {
         let sut = "cat"
         let match = sut
         let expected = sut
         
         #expect(sut.matchedSummary(length: 10, matching: match) == expected)
+    }
+    
+    @Test func if_matching_is_prefix_of_self_then_returns_first_length_characters_of_self() async throws {
+        let sut = "cat on a tin roof"
+        let match = "cat"
+        let length = 6
+        let expected = sut.prefix(length)
+        
+        #expect(sut.matchedSummary(length: length, matching: match) == expected)
     }
 }
