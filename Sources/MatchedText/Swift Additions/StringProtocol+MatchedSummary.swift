189f12a0ae81905cfb3dab688ddbe969f7b3595a
Joseph Wardell <jay@jaywardell.me>
2025-01-06 20:28:32 -0500

    added a precondition to avoid further processing

--- Sources/MatchedText/Swift Additions/StringProtocol+MatchedSummary.swift
+++ Sources/MatchedText/Swift Additions/StringProtocol+MatchedSummary.swift
@@ -7,7 +7,7 @@
 
 extension StringProtocol {
     public func matchedSummary(length: Int, matching filter: any StringProtocol) -> String {
-        guard length > 0 else { return "" }
+        guard length > filter.count else { return "" }
         
         if self.contains(filter) { return String(self) }
         
