8edd66e620794063ab9893d50f1305e0cd64422a
Joseph Wardell <jay@jaywardell.me>
2025-01-06 20:23:07 -0500

    added other base cases

--- Tests/MatchedTextTests/StringProtocol+MatchedSummaryTests.swift
+++ Tests/MatchedTextTests/StringProtocol+MatchedSummaryTests.swift
@@ -10,10 +10,22 @@ import MatchedText
 
 struct String_MatchedSummary_Test {
 
-    @Test func test_empty_returns_empty() async throws {
+    @Test func empty_returns_empty() async throws {
         let sut = ""
         
         #expect(sut.matchedSummary(length: 0, matching: "") == "")
+    }
+
+    @Test func if_length_is_zero_returns_empty() async throws {
+        let sut = "cat"
+        
+        #expect(sut.matchedSummary(length: 0, matching: "cat") == "")
+    }
+
+    @Test func if_mayching_is_empty_returns_empty() async throws {
+        let sut = "cat"
+        
+        #expect(sut.matchedSummary(length: 10, matching: "") == "")
     }
 
 }
